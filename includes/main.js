ab90708d3e2c1eade94b2a93c85004dcf0106e45
Roger Enand <rogerenand@gmail.com>
2019-09-20 18:12:44 -0700

    JS | Final Gallery - Roger Enand

--- includes/main.js
+++ includes/main.js
@@ -18,15 +18,49 @@ var pictures = [
 	'images/pexels-photo-132037.jpeg',
 	'images/pretty.jpg',
 ];
-
+var picturesOG = pictures;
 function initiateApp(){
 	/*advanced: add jquery sortable call here to make the gallery able to be sorted
 		//on change, rebuild the images array into the new order
 	*/
-	makeGallery(pictures);
+
+	// makeGallery(pictures);
 	addModalCloseHandler();
+	var sortableObj = {
+		start: makeGallery(pictures),
+		update: findSortOrder
+	}
+	$('#gallery').sortable(sortableObj); /* makes the pictures sortable and updates the array on change 
+	makeGallery at the start to create the gallery*/
+	var theGalleryFigure = $('#gallery>figure');
+	$(theGalleryFigure).click(displayImage);
 }
+/* --------finds and logs the current sort order and re arranges pictures array ----------*/
+function findSortOrder(){
+	var theOrder = $('#gallery').sortable('toArray',{attribute: 'style'});
+	for (var currentImg = 0; currentImg<theOrder.length;currentImg++){
+		var valToChangeIndex = theOrder[currentImg].lastIndexOf('images/');
+		var changedVal = theOrder[currentImg].slice(valToChangeIndex);
+		valToChangeIndex = changedVal.lastIndexOf('")');
+		changedVal = changedVal.slice(0,valToChangeIndex);
+		theOrder[currentImg] = changedVal;
+	}
+	pictures = theOrder;
+
+	console.log("This is the new pictures array order");
+	console.log(pictures);
+}
+/* ----------makes the gallary---------- */
 function makeGallery(imageArray){
+	var pSize = pictures.length;
+	var target = $('#gallery');
+	for( var fig = 0; fig < pSize; fig++){
+		var bgImg = 'background-image: url('+pictures[fig]+')'
+		var newFig = $('<figure>',{class: 'imageGallery col-xs-12 col-sm-6 col-md-4',style: bgImg, css:{'text-shadow':'1px 1px 2px black',color:'white'} });
+		var newFigCap = $('<figcaption>',{text: pictures[fig].slice(7)});
+		$(newFig).append(newFigCap);
+		$(target).append(newFig);
+	}
 	//use loops and jquery dom creation to make the html structure inside the #gallery section
 
 	//create a loop to go through the images in the imageArray
@@ -39,13 +73,30 @@ function makeGallery(imageArray){
 	// side note: make sure to remove the hard coded html in the index.html when you are done!
 
 }
-
 function addModalCloseHandler(){
+	$('.modal-body>img').click(function(){
+		$('#galleryModal').modal('hide');
+	});
+	
 	//add a click handler to the img element in the image modal.  When the element is clicked, close the modal
 	//for more info, check here: https://www.w3schools.com/bootstrap/bootstrap_ref_js_modal.asp	
 }
-
 function displayImage(){
+	var imgSrc = $(this).css('background-image');
+	imgSrc = imgSrc.slice(imgSrc.lastIndexOf('images/'));
+	imgSrc = imgSrc.slice(0,-2); /* get the lastindexof value and slices everything before it. then slices 2 off the end to remove ") */
+
+	var modTitleIndex = imgSrc.slice(imgSrc.lastIndexOf('.'));
+	var modTitle = imgSrc.slice(0,-modTitleIndex.length); /* finds where the . is on the imgSrc sring and removes that and after */
+	// modTitleIndex = imgSrc.slice(modTitle.lastIndexOf('images/'));
+	modTitle = modTitle.slice(7);
+
+	$('.modal-title').text(modTitle);
+	var imgTarget = $('.modal-body>img');
+	imgTarget.attr('src',imgSrc);/* sets the image src attribute to imgSrc */
+
+	$('#galleryModal').modal('show');
+
 	//find the url of the image by grabbing the background-image source, store it in a variable
 	//grab the direct url of the image by getting rid of the other pieces you don't need
 
